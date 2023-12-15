4ca162446c1645d70ed98af8b2d899f7cedb93a0
mvsyr <123527909+mvsyr@users.noreply.github.com>
2023-12-14 19:39:27 -0500

    Add files via upload

--- Project4/reduceProcess/reduceProcess.cpp
+++ Project4/reduceProcess/reduceProcess.cpp
@@ -1,20 +1,75 @@
 // reduceProcess.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <Windows.h>
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#include "File Management.h"
+#include "C:\Users\moimeme\Downloads\CSE-687-Object-Oriented-Design-Project-4-testing\CSE-687-Object-Oriented-Design-Project-4-testing\Project4\ReduceDLL\ReduceInterface.h"
+
+using std::stringstream;
+using std::vector;
+using std::string;
+using std::wstring;
+using std::to_string;
+using std::getline;
+using std::cout;
+using std::cin;
+using std::endl;
+
+typedef ReduceInterface* (*CREATE_REDUCER) ();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string fileName = "";  // Temporary
+    string fileString = "";  // Temporary
+    string inputDirectory = "";  // Temporary
+    string outputDirectory = "";  // Temporary
+    string tempDirectory = "";  // Temporary
+
+    string reduced_string;
+    
+    FileManagement FileManage(inputDirectory, outputDirectory, tempDirectory); //Create file management class based on the user inputs
+    cout << "FileManagement Class initialized.\n";
+
+    HMODULE reduceDLL = LoadLibraryA("ReduceDLL.dll"); // load dll for library functions
+    if (reduceDLL == NULL) // exit main function if reduceDLL is not found
+    {
+        cout << "Failed to load reduceDLL." << endl;
+        return 1;
+    }
+    
+    CREATE_REDUCER reducerPtr = (CREATE_REDUCER)GetProcAddress(reduceDLL, "CreateReduce");  // create pointer to function to create new Reduce object
+    ReduceInterface* pReducer = reducerPtr();
+
+    //Read from intermediate file and pass data to Reduce class
+    fileString = FileManage.ReadSingleFile(tempDirectory);     //Read single file into single string
+    //cout << "Single file read.\n";
+
+    pReducer->import(fileString);
+    //cout << "String imported by reduce class function and placed in vector.\n";
+
+    pReducer->sort();
+    //cout << "Vector sorted.\n";
+
+    pReducer->aggregate();
+    //cout << "Vector aggregated.\n";
+
+    pReducer->reduce();
+    //cout << "Vector reduced.\n";
+
+    reduced_string = pReducer->vector_export();
+    //cout << "Vector exported to string.\n";
+
+    //Sorted, aggregated, and reduced output string is written into final output file
+    FileManage.WriteToOutputFile(outputDirectory, reduced_string);
+    cout << "Reduced string written to output file.\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
